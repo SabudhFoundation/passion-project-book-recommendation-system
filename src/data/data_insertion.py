788287268819f1af20e1319b19d5ca1732227eb2
Jaykishan2401 <80833188+Jaykishan2401@users.noreply.github.com>
2024-12-28 22:43:47 +0530

    Update data_insertion.py

--- src/data/data_insertion.py
+++ src/data/data_insertion.py
@@ -1,1 +1,15 @@
 # dump final csv in mongo
+import pandas as pd
+from synthetic import generate_synthetic_data
+from src.dbutils.connect_database import ConnectDatabase
+from src.dbutils import dbwrapper
+
+df = pd.read_csv(r"data/processed/final.csv")
+df = df[:2000]
+
+user_book_df = generate_synthetic_data(df)
+
+db_connection = ConnectDatabase()
+
+dbwrapper.insert_documents(db_connection, 'books_data', user_book_df, many=True)
+dbwrapper.insert_documents(db_connection, 'all_books', df, many=True)
