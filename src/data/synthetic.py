d68f69695c9a7c48971d397b18efca850b72355b
Jaykishan2401 <80833188+Jaykishan2401@users.noreply.github.com>
2024-12-28 22:44:04 +0530

    Update synthetic.py

--- src/data/synthetic.py
+++ src/data/synthetic.py
@@ -1,1 +1,31 @@
 # put the function of synthetic data generation and store synthethic data in mongo
+import pandas as pd
+import numpy as np
+
+
+def generate_synthetic_data(df):
+    # Creating a synthetic user dataset
+    num_users = 500
+
+    user_book_list = []
+    u_ids = [i for i in range(1, 1 + num_users)]
+    for i in range(len(df)):
+        n_users = int(50 + (6.5) * np.random.standard_normal())
+        reader_ids = np.random.choice(u_ids, n_users, replace=True)
+        row = df.loc[i]
+        for reader in reader_ids:
+            user_r = row['average_rating'].astype(float)
+            r_scale = (5 - user_r) / 3
+            user_rating = int(np.random.normal(loc=user_r, scale=r_scale))
+            user_book_list.append({
+                "user_id": reader,
+                "isbn": row["isbn"],
+                "title": row["title"],
+                "rating": user_rating,
+                "description": row['description'],
+                "image_url": row['new_image_url'],
+            })
+
+    user_book_df = pd.DataFrame(user_book_list)
+
+    return user_book_df
